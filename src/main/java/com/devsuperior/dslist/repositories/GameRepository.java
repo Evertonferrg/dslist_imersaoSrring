ab3156aa130558a85b670d04ff4f50cb1a6154d5
Everton Ferreira Guedes <evertonferrg@gmail.com>
2025-05-22 15:31:07 -0300

    Move games on list

--- src/main/java/com/devsuperior/dslist/repositories/GameRepository.java
+++ src/main/java/com/devsuperior/dslist/repositories/GameRepository.java
@@ -4,10 +4,11 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.projections.GameMinProjection;
-
+@Repository 
 public interface GameRepository extends JpaRepository<Game, Long>{
 
 	
